bcaed9a7ad8509888b2a9f0f8c59d45330e982b7
ivan <amoremiosa13@gmail.com>
2019-11-24 06:15:40 +0300

    Backup project

--- bot.py
+++ bot.py
@@ -14,23 +14,33 @@ def send_flag(message: Message):
         flag = random.choice(os.listdir("flags/"))
         photo = open('flags/'+flag, 'rb')
         bot.send_photo(message.chat.id, photo, 'What is country?')
+        return flag
+
+
+def del_trash(flag):
         flagsiso = flag.replace(".png", "")
-        filename = 'list-of-iso.txt'
-        with open(filename, 'r') as f:
-            for (i, line) in enumerate(f):
-                if str(flagsiso.upper()) in line:
-                    name_country = linecache.getline('list-of-countries.txt', (i + 1))
-                    bot.reply_to(message, name_country)
-        return name_country
+        return flagsiso
 
 
-@bot.message_handler(func=lambda message: True)
-def checking(message: Message, name_country):
+def line_num_for_phrase_in_file(flagsiso, filename='list-of-iso.txt'):
+    with open(filename, 'r') as f:
+        for (i, line) in enumerate(f):
+            if str(flagsiso.upper()) in line:
+                return i, line
+
+
+def search_name_of_country(i):
+    name_country = linecache.getline('list-of-countries.txt', (i+1))
+    return name_country
+
+
+def checking(message, name_country):
     if message.text == name_country:
         print(name_country)
         bot.reply_to(message, 'You are damn right!')
     else:
         bot.reply_to(message, 'Try again!')
         return
+
 
 bot.polling()
