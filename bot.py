75f7cca46cb9c69a5f97557169af2702c960977b
ivan <amoremiosa13@gmail.com>
2019-11-24 04:46:31 +0300

    All ok, i have name country

--- bot.py
+++ bot.py
@@ -17,32 +17,21 @@ def send_flag(message: Message):
         return flag
 
 
-@bot.message_handler(content_types=['text'])
-def send_message(message: Message, flag):
-    if 'opt' in message.text:
-        bot.send_message(message.chat.id, str(flag))
-        flagvar = flag
-        flagsiso = flagvar.replace(".png", "")
+def del_trash(flag):
+        flagsiso = flag.replace(".png", "")
+        print(flagsiso)
         return flagsiso
 
 
-def del_trash(flag, message):
-    if '1' in message.text:
-        bot.send_message(message.chat.id, str(flag))
-        flagvar = str(flag)
-        flagsiso = flagvar.replace(".png", "")
-    return
-
-
-def line_num_for_phrase_in_file(flagiso, filename='list-of-iso.txt'):
-    with open(filename,'r') as f:
+def line_num_for_phrase_in_file(flagsiso, filename='list-of-iso.txt'):
+    with open(filename, 'r') as f:
         for (i, line) in enumerate(f):
-            if str(flagiso) in line:
+            if str(flagsiso.upper()) in line:
                 return i, line
 
 
 def search_name_of_country(i):
-    name_country = linecache.getline('list-of-countries.txt', i)
+    name_country = linecache.getline('list-of-countries.txt', (i+1))
     return name_country
 
 
