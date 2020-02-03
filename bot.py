47b5f97ee0ea10b3324bff049ccd0ebac0a6169e
hans <amoremiosa13@gmail.com>
2020-02-03 20:14:36 +0200

    Test

--- bot.py
+++ bot.py
@@ -13,40 +13,40 @@ conn = sqlite3.connect('example.db')
 
 
 @bot.message_handler(commands=['start', 'help'])
-def send_welcome(message: Message):
+class Main:
+    def send_welcome(self, message: Message):
+        # sending a picture of country
         flag = random.choice(os.listdir("flags/"))
         photo = open('flags/'+flag, 'rb')
         bot.send_photo(message.chat.id, photo, 'What country is this??')
         return flag
 
-
-@bot.message_handler()
-def del_trash(flag):
+    def del_trash(self, flag):
+        # deleting a part of string with ".png"
         flagsiso = flag.replace(".png", "")
         return flagsiso
 
+    def line_num_for_phrase_in_file(self, flagsiso, filename='list-of-iso.txt'):
+        # returning a number of string with name of image this country
+        with open(filename, 'r') as f:
+            for (i, line) in enumerate(f):
+                if str(flagsiso.upper()) in line:
+                    return i, line
 
-@bot.message_handler()
-def line_num_for_phrase_in_file(flagsiso, filename='list-of-iso.txt'):
-    with open(filename, 'r') as f:
-        for (i, line) in enumerate(f):
-            if str(flagsiso.upper()) in line:
-                return i, line
-
-
-@bot.message_handler()
-def search_name_of_country(i):
-    name_country = linecache.getline('list-of-countries.txt', (i+1))
-    return name_country
+    def search_name_of_country(self, i):
+        # returning name of country by correlation numbers of strings
+        name_country = linecache.getline('list-of-countries.txt', (i+1))
+        return name_country
 
 
-def checking(message, name_country):
-    if message.text == name_country:
-        print(name_country)
-        bot.reply_to(message, 'You are damn right!')
-    else:
-        bot.reply_to(message, 'Try again!')
-        return
+    def checking(self, message, name_country):
+        # checking answer of user with correct name of country
+        if message.text == name_country:
+            print(name_country)
+            bot.reply_to(message, 'You are damn right!')
+        else:
+            bot.reply_to(message, 'Try again!')
+            return
 
 
 bot.polling()
