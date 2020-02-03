106fe3b278112c6f694845424f03055970953a2d
hans <amoremiosa13@gmail.com>
2020-02-03 23:36:37 +0200

    test

--- bot.py
+++ bot.py
@@ -52,9 +52,9 @@ def checking(message):
         send_welcome(message)
     else:
         if message.text.strip().lower() == answers[message.chat.id].lower():
-            bot.reply_to(message, f'You are damn right! It is {answers[message.chat.id]} https://wikipedia.org/wiki/{answers[message.chat.id].replace(" ", "+")}')
+            bot.reply_to(message, f'You are damn right!  It is {answers[message.chat.id]} https://wikipedia.org/wiki/{answers[message.chat.id].replace(" ", "+")}')
         else:
-            bot.reply_to(message, f'Try again! It is {answers[message.chat.id]} https://wikipedia.org/wiki/{answers[message.chat.id].replace(" ", "_")}')
+            bot.reply_to(message, f'Try again!  It is {answers[message.chat.id]} https://wikipedia.org/wiki/{answers[message.chat.id].replace(" ", "_")}')
         return
 
 
