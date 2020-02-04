d5ec5b7bbaa29bdee0be71c255501234a83256ef
hans <amoremiosa13@gmail.com>
2020-02-05 01:12:43 +0200

    change button from "Next" to "I give up" (fix problems)

--- bot.py
+++ bot.py
@@ -52,7 +52,7 @@ def search_name_of_country(flag):
 
 
 @bot.message_handler(content_types=['text'])
-def checking(message):
+def checking(message: Message):
     key = types.InlineKeyboardMarkup()
     itembtn = types.InlineKeyboardButton(text="Next", callback_data="next")
     key.add(itembtn)
@@ -68,8 +68,16 @@ def callback_inline(call):
     if call.data == "next":
         send_welcome(call.message)
     elif call.data == "give_up":
-        checking(call.message)
+        give_up(call.message)
     return
+
+
+@bot.message_handler()
+def give_up(message: Message):
+    key = types.InlineKeyboardMarkup()
+    itembtn = types.InlineKeyboardButton(text="Next", callback_data="next")
+    key.add(itembtn)
+    bot.send_message(message.chat.id, f'Really? It is {answers[message.chat.id]} https://wikipedia.org/wiki/{answers[message.chat.id].replace(" ", "_")}', reply_markup=key)
 
 
 if __name__ == '__main__':
