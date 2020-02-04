85079133478dd14d6bddc33a373085359789f545
hans <amoremiosa13@gmail.com>
2020-02-04 23:32:18 +0200

    fix problem with token

--- bot.py
+++ bot.py
@@ -3,11 +3,11 @@ import os, random
 from telebot.types import Message
 import linecache
 from telebot import types
-from telebot import logger
 #import sqlite3
 
 
-token = os.getenv("token")
+with open("token.txt") as f:
+    token = f.read().strip()
 TOKEN = f'{token}'
 
 
@@ -21,13 +21,12 @@ answers = {}
 def send_welcome(message: Message):
     # sending a picture of country
     key = types.InlineKeyboardMarkup()
-    itembtn1 = types.InlineKeyboardButton(text="Next", callback_data="next")
-    key.add(itembtn1)
+    itembtn = types.InlineKeyboardButton(text="Next", callback_data="next")
+    key.add(itembtn)
     flag = random.choice(os.listdir("flags/"))
     answers[message.chat.id] = search_name_of_country(flag)
     photo = open('flags/'+flag, 'rb')
-    photo = bot.send_photo(message.chat.id, photo, 'What country is this?', reply_markup=key)
-    bot.register_next_step_handler(photo, checking)
+    bot.send_photo(message.chat.id, photo, 'What country is this?', reply_markup=key)
 
 
 def del_trash(flag):
@@ -54,16 +53,11 @@ def search_name_of_country(flag):
 
 @bot.message_handler(content_types=['text'])
 def checking(message):
-    if message.text == '/start':
-        send_welcome(message)
-    elif message.text == 'Next':
-        send_welcome(message)
+    if message.text.strip().lower() == answers[message.chat.id].lower():
+        bot.reply_to(message, f'You are damn right! It is {answers[message.chat.id]} https://wikipedia.org/wiki/{answers[message.chat.id].replace(" ", "_")}')
     else:
-        if message.text.strip().lower() == answers[message.chat.id].lower():
-            bot.reply_to(message, f'You are damn right! It is {answers[message.chat.id]} https://wikipedia.org/wiki/{answers[message.chat.id].replace(" ", "_")}')
-        else:
-            bot.reply_to(message, f'Try again! It is {answers[message.chat.id]} https://wikipedia.org/wiki/{answers[message.chat.id].replace(" ", "_")}')
-        return
+        bot.reply_to(message, f'Try again! It is {answers[message.chat.id]} https://wikipedia.org/wiki/{answers[message.chat.id].replace(" ", "_")}')
+    return
 
 
 @bot.callback_query_handler(func=lambda call: True)
